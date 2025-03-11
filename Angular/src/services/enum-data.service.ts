afdbaf46361c03c35920df68b6081b7f715d048e
Sima <m07213984@gmail.com>
2025-03-11 14:07:08 +0200

    about,sign in- Angular|dto, get by id,log in-C#

--- Angular/src/services/enum-data.service.ts
+++ Angular/src/services/enum-data.service.ts
@@ -1,3 +1,76 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+
+// @Injectable({ providedIn: 'root' })
+// export class EnumDataService {
+//   private apiUrl = 'https://localhost:7100/api/enums'; // שימי לב לכתובת שלך
+
+//   constructor(private http: HttpClient) {}
+
+//   getGenders(): Observable<any[]> {
+//     // console.log("/Gender`)",this.http.get<any[]>(`${this.apiUrl}/Gender`));
+//     return this.http.get<any[]>(`${this.apiUrl}/Gender`);
+//   }
+
+//   getLanguages(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Language`);
+//   }
+
+//   getInterests(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Interests`);
+//   }
+
+//   getAgeGroups(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/AgeGroup`);
+//   }
+
+//   getAreas(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Area`);
+//   }
+
+//   getCities(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/City`);
+//   }
+
+//   getDaysOfWeek(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/DayOfWeek`);
+//   }
+
+//   getHospitals(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Hospital`);
+//   }
+
+//   getPatients(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Patient`);
+//   }
+
+//   getPatientPostModels(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/PatientPostModel`);
+//   }
+
+//   getReligiosities(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Religiosity`);
+//   }
+
+//   getTimes(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Time`);
+//   }
+
+//   getVolunteers(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Volunteer`);
+//   }
+
+//   getWards(): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/Ward`);
+//   }
+
+//   // אפשר להוסיף מתודה גנרית אם ברצונך לגשת ל-Enums נוספים בצורה דינמית
+//   getEnumData(enumName: string): Observable<any[]> {
+//     return this.http.get<any[]>(`${this.apiUrl}/${enumName}`);
+//   }
+// }
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,7 +83,7 @@ export class EnumDataService {
   constructor(private http: HttpClient) {}
 
   getGenders(): Observable<any[]> {
-    // console.log("/Gender`)",this.http.get<any[]>(`${this.apiUrl}/Gender`));
+    console.log("/Gender`)",this.http.get<any[]>(`${this.apiUrl}/Gender`));
     return this.http.get<any[]>(`${this.apiUrl}/Gender`);
   }
 
